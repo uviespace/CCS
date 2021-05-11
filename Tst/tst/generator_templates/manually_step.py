eb36fa1564fa54ae3a9cd99335c08f79b02739b8
moeslinged94 <Feststelltaste1>
2021-05-11 15:29:47 +0200

    New branch is now up to date with "old" OBSW git, all previous commits can be found there

--- Tst/tst/generator_templates/manually_step.py
+++
@@ -1,4 +0,0 @@
-
-#! CCS.BREAKPOINT
-# Step $testStepNumber: $testStepDescription Comment: $testStepComment
-threading.Thread(target=testinstance.step_$testStepNumber, kwargs={'ccs': ccs, 'pool_name': pool_name}, daemon=True).start()
