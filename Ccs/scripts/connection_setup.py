4e8d86f5ab0277a2a88d2007d908c4f99dfc1d32
Marko Mecina <marko.mecina@univie.ac.at>
2022-02-16 13:47:47 +0100

    add basic CCS template scripts

---
+++ Ccs/scripts/connection_setup.py
@@ -0,0 +1,15 @@
+# Basic connection setup to IASW sim via PLM SW sim
+
+### Poolmanager ###
+cfl.start_pmgr()
+
+# PLM connection
+cfl.connect('LIVE', '', 5570, protocol='PUS')
+cfl.connect_tc('LIVE', '', 5571, protocol='PUS')
+
+### Poolviewer ###
+cfl.start_pv()
+
+#! CCS.BREAKPOINT
+cfl.start_monitor('LIVE')
+# cfl.set_monitor('LIVE')
