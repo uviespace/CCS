3b96554bfa224b6de3c2e8caf28f28bd296a7a51
Marko Mecina <marko.mecina@univie.ac.at>
2022-08-16 13:51:55 +0200

    cleanup of CCS IPython startup config

--- Ccs/.ipycfg.py
+++ Ccs/.ipycfg.py
@@ -1,3 +1,6 @@
 import gi
-import dbus
 gi.require_version('Gtk', '3.0')
+
+# disable autosuggest in IPython >= 8
+# c = get_config()
+# c.TerminalInteractiveShell.autosuggestions_provider = None
--- Ccs/.ipyloadcfg.py
+++ Ccs/.ipyloadcfg.py
@@ -10,47 +10,15 @@ import ccs_function_lib as cfl
 
 gi.require_version('Gtk', '3.0')
 
-# try:
-#     with open('.sharedvariables.bin', 'rb') as fdesc:
-#         shared = pickle.load(fdesc)
-#     cfg = shared['cfg']
-# except:
-#     pass
-# finally:
-#     fdesc.close()
-#logger = shared['logger']
-
-#sys.stderr = io.StringIO()
-#Connect to every open DBus,
-dbus_type = dbus.SessionBus()
 import confignator
 cfg = confignator.get_config()
 
-def kwargs(arguments={}):
+dbus_type = dbus.SessionBus()
+
+
+def kwargs(arguments=None):
+
+    if arguments is None:
+        arguments = {}
+
     return dbus.Dictionary({'kwargs': dbus.Dictionary(arguments, signature='sv')})
-
-
-'''
-# Now done in editor.py
-# Connect to all open applications
-try:
-    Bus_Name_poolviewer = cfg.get('dbus_names', 'poolviewer')
-    pv = dbus_type.get_object(Bus_Name_poolviewer, '/MessageListener')
-except:
-    pass
-try:
-    Bus_Name_poolmgr = cfg.get('dbus_names', 'poolmanager')
-    pmgr = dbus.SessionBus().get_object(Bus_Name_poolmgr, '/MessageListener')
-except:
-    pass
-try:
-    Bus_Name_monitor = cfg.get('dbus_names', 'monitor')
-    monitor = dbus.SessionBus().get_object(Bus_Name_monitor, '/MessageListener')
-except:
-    pass
-try:
-    Bus_Name_plotter = cfg.get('dbus_names', 'plotter')
-    plotter = dbus.SessionBus().get_object(Bus_Name_plotter, '/MessageListener')
-except:
-    pass
-'''
