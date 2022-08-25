fd189ceaa09434569cbe27e01327f5c4868f7235
Marko Mecina <marko.mecina@univie.ac.at>
2022-08-25 14:30:20 +0200

    update CCS startup script

--- Ccs/ccs.py
+++ Ccs/ccs.py
@@ -5,6 +5,7 @@ import sys
 import editor
 import gi
 import DBus_Basic
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib
 from dbus.mainloop.glib import DBusGMainLoop
@@ -20,35 +21,22 @@ def run():
     global files_to_open
 
     win = editor.CcsEditor()
-    # cfl.start_editor()
 
-    if cfg.has_option('init', 'init_script'):
-        init_script = cfg.get('init', 'init_script')
+    if cfg.has_option('ccs-init', 'init_script'):
+        init_script = cfg.get('ccs-init', 'init_script')
         if init_script != '':
             init_cmd = 'exec(open("{}","r").read())\n'.format(init_script)
-            # win.ipython_view.text_buffer.insert_at_cursor(init_cmd, len(init_cmd))
-            # win.ipython_view._processLine()
             win.ipython_view.feed_child(init_cmd, len(init_cmd))
 
-    given_cfg = None
-    for i in sys.argv:
-        if i.endswith('.cfg'):
-            given_cfg = i
-    if given_cfg:
-        cfg = confignator.get_config(file_path=given_cfg)
-    else:
-        cfg = confignator.get_config(file_path=confignator.get_option('config-files', 'ccs'))
-    # pv = TMPoolView(cfg)
     DBusGMainLoop(set_as_default=True)
     if files_to_open:
         for fname in files_to_open:
             win.open_file(fname)
     else:
-        win.open_file(os.path.join(confignator.get_option('paths', 'ccs'),'getting_started.py'))
+        win.open_file(os.path.join(confignator.get_option('paths', 'ccs'), 'getting_started.py'))
 
-    Bus_Name = cfg.get('ccs-dbus_names', 'editor')
-
-    DBus_Basic.MessageListener(win, Bus_Name, *sys.argv)
+    bus_name = cfg.get('ccs-dbus_names', 'editor')
+    DBus_Basic.MessageListener(win, bus_name, *sys.argv)
 
 
 if __name__ == "__main__":
