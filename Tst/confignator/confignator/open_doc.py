f0c9443da9335c730ea46db0452eca664931624c
Marko Mecina <marko.mecina@univie.ac.at>
2022-08-25 14:11:20 +0200

    make opening documentation browser-agnostic

--- Tst/confignator/confignator/open_doc.py
+++ Tst/confignator/confignator/open_doc.py
@@ -3,13 +3,17 @@
 Opens the documentation of the confignator package in firefox.
 """
 import os
+import webbrowser
 from confignator import config
 
 
 def open_documentation_in_firefox():
     docu = config.get_option('confignator-paths', 'docu')
-    command = 'firefox ' + docu
-    os.system(command)
+
+    if not os.path.isfile(docu):
+        raise FileNotFoundError('Documentation not found: {}. Try rebuilding it.'.format(docu))
+
+    webbrowser.open(docu)
 
 
 if __name__ == '__main__':
