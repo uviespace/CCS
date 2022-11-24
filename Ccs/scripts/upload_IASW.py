8d7dc51626ca1d4db1d85c632dd3a1e08e9ed85f
Marko Mecina <marko.mecina@univie.ac.at>
2022-11-24 14:40:04 +0100

    update SREC upload script, again

--- Ccs/scripts/upload_IASW.py
+++ Ccs/scripts/upload_IASW.py
@@ -19,7 +19,7 @@ cfl.upload_srec(srecfile, memid, mem_addr, segid, pool_name='LIVE', max_pkt_size
 srecfile = '/path/to/srec'  # SREC filename
 memid = 'EEPROM'
 tc_cmd = 'SES CMD_Memory_Load'
-dlen, dcrc = srec_direct(srecfile, memid, pool_name='LIVE', max_pkt_size=1024, tcname=tc_cmd, sleep=0.125, byte_align=2)
+dlen, dcrc = cfl.srec_direct(srecfile, memid, pool_name='LIVE', max_pkt_size=200, tcname=tc_cmd, sleep=0.125, byte_align=2)
 
 
 #! CCS.BREAKPOINT
