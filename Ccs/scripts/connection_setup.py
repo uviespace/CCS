07e8afcbc7c23361da6914d9ca7f200011acb6be
pasetti <pasetti@pnp-software.com>
2024-02-16 12:11:25 +0100

    Deleted irrelevant commands

--- Ccs/scripts/connection_setup.py
+++ Ccs/scripts/connection_setup.py
@@ -15,7 +15,3 @@ cfl.start_pv()
 cfl.start_monitor('LIVE')
 
 
-L020003NumOfParams = 1  # UCAH0054
-L020003ParamId = 178349  # UCAH0055
-L020003OnbParamVal = bytes([11,22,33,44])  # UCAH0056
-cfl.Tcsend_DB('L020003SetParamValues', L020003NumOfParams, L020003ParamId, L020003OnbParamVal, pool_name='LIVE')
