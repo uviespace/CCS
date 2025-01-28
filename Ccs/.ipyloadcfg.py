2ebf4ccb89361d926c62153519d72bea2391e39d
Marko Mecina <marko.mecina@univie.ac.at>
2025-01-28 14:20:19 +0100

    update communications module

--- Ccs/.ipyloadcfg.py
+++ Ccs/.ipyloadcfg.py
@@ -3,15 +3,12 @@
 import sys
 import io
 import os
-# import gi
 import dbus
 import dbus.service
 import logging
 import ccs_function_lib as cfl
 
 sys.path.append(cfl.cfg.get('paths', 'ccs'))
-
-# gi.require_version('Gtk', '3.0')
 
 # import confignator
 # cfg = confignator.get_config(check_interpolation=False)
