a572bc18f76d9db8fd567ddce71a1b279100453c
Christopher Granabetter <christopher.granabetter@univie.ac.at>
2025-05-07 21:20:22 +0200

    updated json to java converter

--- Ccs/tools/java_test_conversion/convert_json_to_java.py
+++ Ccs/tools/java_test_conversion/convert_json_to_java.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 """
+Script to convert JSON test specifications into Java test sequence files.
+
+This script reads JSON files containing test specifications, processes the data, 
+and generates corresponding Java test sequence files. The generated Java files 
+are structured to work with the SimTG framework and include preconditions, 
+test steps, and postconditions.
 
 author: Christopher Granabetter
 date: 05.05.2025
@@ -18,9 +24,10 @@ cfl.add_tst_import_paths()
 from time import time
 
 
-MIB_VERSION = '1.8.1'
-
 def make_start(filename):
+    """
+    Generate the header and initial setup for the Java test sequence file.
+    """
     return f"""package pl_cbk_fgs_ariel.ut;
 
 import simtg.simops.base.SimopsException;
@@ -85,10 +92,12 @@ END = """
 """
 
 def send_bytes(byte_array):
-
+    """
+    Generate Java code to send a byte array as a SpaceWire packet.
+    """
     print("Sending bytes:", byte_array, type(byte_array))
 
-    # dummy pkt byte string SpW packets not included
+    # Generate Java code for sending the byte array
     pkt_byte_str = "".join([f"pkt[{i+4}] = (byte) {hex(byte)};\n            " for i, byte in enumerate(byte_array)])
 
     return f"""
@@ -129,26 +138,17 @@ def log_note(desc):
     return f"""
             sim.insertLog("{desc}");"""
 
-def parse_command_file(file_path):
-    try:
-        with open(file_path, "r") as f:
-            lines = f.readlines()
-    except FileNotFoundError:
-        print(f"Error: File {file_path} not found.")
-        return None
-    except IOError as e:
-        print(f"Error reading file {file_path}: {e}")
-        return None
-
-
 def sequence_counter(data):
+    """
+    Calculate the sequence counter from the given data.
+    """
     lower6 = data[2] & 0x3F
     next_byte = data[3]
     return (lower6 << 8) | next_byte
 
 def execute_code(code, namespace, data, step):
     """
-    Execute the given code in the provided namespace.
+    Execute the given Python code in the provided namespace.
     """
     try:
         print("TEST: " + data['_name'])
@@ -162,7 +162,9 @@ def execute_code(code, namespace, data, step):
         raise
 
 def run(jfile, output_path):
-
+    """
+    Process a JSON file and generate a corresponding Java test sequence file.
+    """
     if os.path.isfile(jfile):
         data = json.load(open(jfile, 'r'))
     else:
@@ -170,9 +172,11 @@ def run(jfile, output_path):
 
     date = datetime.datetime.now().strftime('%Y-%m-%d')
 
+    # generate the Java file name and produce start of the Java script
     java_file = os.path.basename(jfile).replace(".json","").replace("-", "_").replace(".", "") + ".java"
     script = make_start(java_file[:-5])
 
+    # add metadata and preconditions to the Java script
     script += log_note('--------------------------------------------')
     script += log_note(data['_name'])
     script += log_note(data['_description'])
@@ -189,11 +193,15 @@ def run(jfile, output_path):
     script += log_note('PRECONDITIONS: {}'.format(data['_precon_descr']))
     # script += '{}\n\n\n'.format(data['_precon_code'].strip())  # Add the precondition code
 
+    # initialize the namespace for executing Python code
     namespace = {"cfl": cfl,
                  "tcs": [],
                  "time": time,}
-    # reset all counters for new test
+    
+    # reset all counters for the new test
     exec("cfl.counters.clear()", namespace)
+    
+    # process each step in the test sequence
     for step in data['sequences'][0]['steps']:
         comment = log_note('COMMENT: {}'.format(step['_step_comment'])) if step['_step_comment'] != '' else ''
         cmd_code = step['_command_code'].strip()
@@ -203,14 +211,17 @@ def run(jfile, output_path):
         txt += log_note('{}'.format(step['_description']).replace('\n', ''))
 
         print("\n\n")
+        
+        # generate TC bytes from python code, if code exists
         if cmd_code == '':
             print("TEST: " + data['_name'])
             print("STEP: " + step['_step_number'])
             print("DESCRIPTION: " + step['_description'])
-            print("CODE:\n" + cmd_code)
+            print("CODE: No Code\n")
             print("VERIFICATION: " + step['_verification_description'] + "\n")
             result = ''
         else:
+            # replace TCsend_DB with Tcbuild to generate the TC bytes and suppress sending
             cmd_code = cmd_code.replace("cfl.Tcsend_DB", "tc, tc_para = cfl.Tcbuild").replace("sent = cfl._tcsend_common", "# sent = cfl._tcsend_common")
             cmd_code_lines = cmd_code.splitlines()
             skip_i = None
@@ -221,12 +232,14 @@ def run(jfile, output_path):
 
                 indent = re.match(r"\s*", line).group()
                 if "Tcbuild" in line:
+                    # make special case for illegal Source ID, i.e. sequence counter is otherwise out of sync
                     if not "wrong Source ID" in step['_description']:
                         cmd_code_lines.insert(i_code_lines+1, indent + "try: cfl.counters[int(str(apid), 0)] += 1\n" + indent + "except: cfl.counters[int(str(tc_para[-1]), 0)] += 1")
                         skip_i = i_code_lines + 1
                         
             cmd_code = "\n".join(cmd_code_lines) + "\n"
 
+            # look for for-loops and save every tc
             if ("\nfor" in cmd_code or cmd_code.startswith("for")) and "    tc" in cmd_code or "\ttc" in cmd_code:
                 cmd_code = "tcs = []\n" + cmd_code
                 cmd_code += "    tcs.append(tc)"
@@ -236,6 +249,7 @@ def run(jfile, output_path):
                 execute_code(cmd_code, namespace, data, step)
                 result = namespace["tc"] if "tc" in namespace else None
         
+        # show result and generate byte packet in Java script
         print("Returned:", result)
         if isinstance(result, bytes):
             txt += send_bytes(result)
@@ -248,7 +262,6 @@ def run(jfile, output_path):
                     txt += send_bytes(tc[0])
                 else:
                     txt += '\n            // No bytes to send'
-                #txt += send_bytes(tc)
         else:
             txt += '\n            // No bytes to send'
 
@@ -261,27 +274,38 @@ def run(jfile, output_path):
     script += log_note('POSTCONDITIONS: {}'.format(data['_postcon_descr']))
     # script += data['_postcon_code'].strip()  # Add the postcondition code
 
+    # close the Java script
     script += END
 
+    # write all Java lines to file
     with open(os.path.join(output_path, java_file), 'w') as fd:
         fd.write(script)
 
 
-
 if __name__ == '__main__':
 
-    not_functional_files = ["/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-5_1-TS-0.2.json",   # ValueError: Range check failed - Invalid parameter value for event_id: EVT_CMD_INV_APID
-                            "/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-20_1-TS-0.2.json",  # KeyError: "1"
-                            "/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-9_1-TS-0.2.json",   # struct.error: bad char in struct format
-                            "/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-213_1-TS-0.2.json", # not readys
-                            "/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-212_1-TS-0.2.json", # struct.error: 'H' format requires 0 <= number <= 65535
-                            "/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-194_1-TS-0.2.json", # ValueError: Range check failed - Invalid parameter value for AlgoId: 1 [valid: TC_ALGO]
-                            "/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-3_1-TS-0.2.json",   # STEP 28 not possible to generate in CCS
-                            "/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-198_1-TS-0.2.json", # ValueError: Range check failed - Invalid parameter value for ProcId: WRONG [valid: CAL_FULL | CHECKOUT_PR | DWN_TRANS_PR | OPER_PR]
-                            ] 
+    ## ARIEL specific
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # dynamically determine the home directory
+    home_dir = os.path.expanduser("~")
 
-    json_file_path = r"/home/christopher/FSW/Documents/testspec/tst/FFT_draft_spec02/IASW-SRV-1_1-TS-0.2.json"
-    # Check if the input path is provided as a command-line argument
+    # define the base path for the files relative to the home directory
+    base_path = os.path.join(home_dir, "FSW/Documents/testspec/tst/FFT_draft_spec02")
+
+    # special cases for not working files
+    not_functional_files = [
+        os.path.join(base_path, "IASW-SRV-5_1-TS-0.2.json"),   # ValueError: Range check failed - Invalid parameter value for event_id: EVT_CMD_INV_APID
+        os.path.join(base_path, "IASW-SRV-20_1-TS-0.2.json"),  # KeyError: "1"
+        os.path.join(base_path, "IASW-SRV-9_1-TS-0.2.json"),   # struct.error: bad char in struct format
+        os.path.join(base_path, "IASW-SRV-213_1-TS-0.2.json"), # Not ready
+        os.path.join(base_path, "IASW-SRV-212_1-TS-0.2.json"), # struct.error: 'H' format requires 0 <= number <= 65535
+        os.path.join(base_path, "IASW-SRV-194_1-TS-0.2.json"), # ValueError: Range check failed - Invalid parameter value for AlgoId: 1 [valid: TC_ALGO]
+        os.path.join(base_path, "IASW-SRV-3_1-TS-0.2.json"),   # STEP 28 not possible to generate in CCS
+        os.path.join(base_path, "IASW-SRV-198_1-TS-0.2.json"), # ValueError: Range check failed - Invalid parameter value for ProcId: WRONG [valid: CAL_FULL | CHECKOUT_PR | DWN_TRANS_PR | OPER_PR]
+    ]
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # check if the input path is provided as a command-line argument
     if len(sys.argv) < 2:
         print("Usage: python convert_json_to_java.py <input_path>")
         sys.exit(1)
